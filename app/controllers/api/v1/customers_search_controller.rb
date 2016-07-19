0303c73dc4ce967e897edda7134ee7bcfe279816
Anna Weisbrodt <aweisbro@gmail.com>
2016-07-18 21:50:49 -0600

    add search controllers

--- app/controllers/api/v1/customers_search_controller.rb
+++ app/controllers/api/v1/customers_search_controller.rb
@@ -1,6 +1,16 @@
 class Api::V1::CustomersSearchController < ApiBaseController
   respond_to :json, :xml
 
+  def index
+    if params[:first_name]
+      respond_with Customer.where("lower(first_name) = ?", params[:first_name].downcase)
+    elsif params[:last_name]
+      respond_with Customer.where("lower(last_name) = ?", params[:last_name].downcase)
+    else
+      respond_with Customer.where(customer_params)
+    end
+  end
+  
   def show
     if params[:first_name]
       respond_with Customer.where("lower(first_name) = ?", params[:first_name].downcase).first
@@ -11,15 +21,6 @@ class Api::V1::CustomersSearchController < ApiBaseController
     end
   end
 
-  def index
-    if params[:first_name]
-      respond_with Customer.where("lower(first_name) = ?", params[:first_name].downcase)
-    elsif params[:last_name]
-      respond_with Customer.where("lower(last_name) = ?", params[:last_name].downcase)
-    else
-      respond_with Customer.where(customer_params)
-    end
-  end
 
 private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
-      resources :transactions, only: [:index, :show]
+
+      resources :merchants, only: [:index, :show] do
+        collection do
+          get 'find', to: 'merchants_search#show'
+          get 'find_all', to: 'merchants_search#index'
+        end
+      end
+
+      resources :transactions, only: [:index, :show] do
+        collection do
+          get 'find', to: 'transactions_search#show'
+          get 'find_all', to: 'transactions_search#index'
+        end
+      end
 
       resources :customers, only: [:index, :show] do
         collection do
