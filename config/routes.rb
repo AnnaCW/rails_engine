b4cd3b9c418344d64c8e6d33da90914081295d6e
riizu <rgbatty@outlook.com>
2016-07-19 16:59:52 -0600

    Fix search and random functions to match new controller organization

--- config/routes.rb
+++ config/routes.rb
@@ -4,40 +4,57 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do
         resources :invoices, only: [:index]
         resources :items, only: [:index]
-        resources :random, only: [:index]
-        resources :search, only: [:index, :show]
+
+        get 'find', to: 'merchants/search#show', :on => :collection
+        get 'find_all', to: 'merchants/search#index', :on => :collection
+        get 'random', to: 'merchants/random#index', :on => :collection
       end
+
       resources :transactions, only: [:index, :show] do
         resources :invoices, only: [:index]
-        resources :random, only: [:index]
-        resources :search, only: [:index, :show]
+
+        get 'find', to: 'transactions/search#show', :on => :collection
+        get 'find_all', to: 'transactions/search#index', :on => :collection
+        get 'random', to: 'transactions/random#index', :on => :collection
       end
+
       resources :customers, only: [:index, :show] do
         resources :invoices, only: [:index]
         resources :transactions, only: [:index]
-        resources :random, only: [:index]
-        resources :search, only: [:index, :show]
+
+        get 'find', to: 'customers/search#show', :on => :collection
+        get 'find_all', to: 'customers/search#index', :on => :collection
+        get 'random', to: 'customers/random#index', :on => :collection
       end
+
       resources :invoices, only: [:index, :show] do
         resources :customers, only: [:index]
         resources :invoice_items, only: [:index]
         resources :items, only: [:index]
         resources :transactions, only: [:index]
         resources :merchants, only: [:index]
-        resources :random, only: [:index]
-        resources :search, only: [:index, :show]
+
+        get 'find', to: 'invoices/search#show', :on => :collection
+        get 'find_all', to: 'invoices/search#index', :on => :collection
+        get 'random', to: 'invoices/random#index', :on => :collection
       end
+
       resources :items, only: [:index, :show] do
         resources :invoice_items, only: [:index]
         resources :merchants, only: [:index]
-        resources :random, only: [:index]
-        resources :search, only: [:index, :show]
+
+        get 'find', to: 'items/search#show', :on => :collection
+        get 'find_all', to: 'items/search#index', :on => :collection
+        get 'random', to: 'items/random#index', :on => :collection
       end
+      
       resources :invoice_items, only: [:index, :show] do
         resources :invoices, only: [:index]
         resources :items, only: [:index]
-        resources :random, only: [:index]
-        resources :search, only: [:index, :show]
+
+        get 'find', to: 'invoice_items/search#show', :on => :collection
+        get 'find_all', to: 'invoice_items/search#index', :on => :collection
+        get 'random', to: 'invoice_items/random#index', :on => :collection
       end
     end
   end
