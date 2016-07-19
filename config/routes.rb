5d14d770eb02e72aafa041793a4d4a0b64ed506a
Anna Weisbrodt <aweisbro@gmail.com>
2016-07-18 20:49:14 -0600

    add find and find all for customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :merchants, only: [:index, :show], defaults: {format: :json}
-      resources :transactions, only: [:index, :show], defaults: {format: :json}
-      resources :customers, only: [:index, :show], defaults: {format: :json}
+      resources :merchants, only: [:index, :show]
+      resources :transactions, only: [:index, :show]
+
+      resources :customers, only: [:index, :show] do
+        collection do
+          get 'find', to: 'customers_search#show'
+          get 'find_all', to: 'customers_search#index'
+        end
+      end
+
     end
   end
 end
