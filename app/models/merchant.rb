5c7ac21b62abb59a2bd8e0668d1c4b1316a0d724
Anna Weisbrodt <aweisbro@gmail.com>
2016-07-20 11:28:10 -0600

    merch rev and rev by date

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,12 @@ class Merchant < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   has_many :items
   has_many :invoices
+
+  def revenue
+    invoices.joins(:invoice_items).sum('quantity * unit_price')
+  end
+
+  def revenue_by_date(invoice_date)
+    invoices.joins(:invoice_items).where(created_at: invoice_date).sum('quantity * unit_price')
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,4 +6,28 @@ RSpec.describe Merchant, type: :model do
   it { should validate_presence_of (:name) }
   it { should validate_uniqueness_of (:name) }
 
+  it "returns total revenue for merchant" do
+    merchant = create(:merchant)
+    invoice_1, invoice_2 = create_list(:invoice, 2, merchant: merchant)
+    invoice_item_1 = create(:invoice_item, invoice: invoice_1, quantity: 2, unit_price: 3)
+    invoice_item_2 = create(:invoice_item, invoice: invoice_2, quantity: 1, unit_price: 2)
+
+    other_merchant = create(:merchant)
+    invoice_3 = create(:invoice, merchant: other_merchant)
+    invoice_item_3 = create(:invoice_item, invoice: invoice_3, quantity: 2, unit_price: 3)
+
+    expect(merchant.revenue).to eq(8)
+  end
+
+  it "returns revenue for merchant for given date" do
+    merchant = create(:merchant)
+    invoice_1, invoice_2 = create_list(:invoice, 2, merchant: merchant, created_at: "2012-03-27T14:53:59.000Z")
+    invoice_item_1 = create(:invoice_item, invoice: invoice_1, quantity: 2, unit_price: 3)
+    invoice_item_2 = create(:invoice_item, invoice: invoice_2, quantity: 1, unit_price: 2)
+    invoice_3 = create(:invoice, merchant: merchant, created_at: "2015-03-27T14:53:59.000Z")
+    invoice_item_3 = create(:invoice_item, invoice: invoice_3, quantity: 2, unit_price: 3)
+
+    expect(merchant.revenue_by_date("2012-03-27T14:53:59.000Z")).to eq(8)
+  end
+
 end
