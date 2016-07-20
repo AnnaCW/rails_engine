c156512ca653b493833d9f2e522c508ec82640b7
riizu <rgbatty@outlook.com>
2016-07-20 12:02:16 -0600

    Add method to find a merchant's customers with pending invoices

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,8 @@ class Merchant < ActiveRecord::Base
   validates_uniqueness_of :name, :case_sensitive => false
   has_many :items
   has_many :invoices
+
+  def customers_with_pending_invoices
+    Customer.joins(:invoices).where(invoices: {status: "pending"})
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe Merchant, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name).ignoring_case_sensitivity }
 
+  it "Finds customers with pending invoices" do
+    merchant = create(:merchant)
+    customer = create(:customer)
+    create_list(:invoice, 3, customer: customer, merchant: merchant,
+                status: "pending")
+
+    result = merchant.customers_with_pending_invoices
+
+    expect(result.count).to eq 3
+    expect(result[0].invoices[0].status).to eq "pending"
+  end
 end
