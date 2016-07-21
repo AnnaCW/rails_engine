42081c596bba1cc240d83ce86edf6012290c7747
Anna Weisbrodt <aweisbro@gmail.com>
2016-07-20 20:07:14 -0600

    remove validations and validation tests

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,6 @@ class Item < ActiveRecord::Base
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
-  validates_presence_of :name
-  validates_presence_of :description
-  validates_presence_of :unit_price
-  validates_presence_of :merchant
 
   def self.most_items(num)
     Item.joins(:invoice_items).group('items.id').order('sum(invoice_items.quantity) desc').limit(num)
