750d15175e9792453e6d712085858b256e56a0dd
riizu <rgbatty@outlook.com>
2016-07-20 19:47:41 -0600

    fix price display

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,3 +1,7 @@
 class InvoiceItemSerializer < ActiveModel::Serializer
   attributes :id, :quantity, :unit_price, :invoice_id, :item_id
+
+  def unit_price
+    "#{object.unit_price.to_s.insert(-3,".")}"
+  end
 end
