bff92b7d2b6e8343e0edad0053d273d1e06f2181
riizu <rgbatty@outlook.com>
2016-07-20 20:02:24 -0600

    Fix price formatting to account for smaller denominations

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -2,6 +2,14 @@ class InvoiceItemSerializer < ActiveModel::Serializer
   attributes :id, :quantity, :unit_price, :invoice_id, :item_id
 
   def unit_price
-    "#{object.unit_price.to_s.insert(-3,".")}"
+    final_string = object.unit_price.to_s
+
+    if final_string.length == 3
+      final_string += "0"
+    elsif final_string == 2
+      final_string += "00"
+    end
+
+    final_string
   end
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -2,6 +2,16 @@ class ItemSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :unit_price, :merchant_id
 
   def unit_price
-    "#{object.unit_price.to_s.insert(-3,".")}"
+    formatted_price = format_price(object.unit_price.to_s)
+    formatted_price.insert(-3, ".")
+  end
+
+  def format_price(string)
+    if string.length == 2
+      string += "00"
+    elsif string.length == 1
+      string += "000"
+    end
+    string
   end
 end
