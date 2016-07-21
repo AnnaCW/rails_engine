864e55a4fd9dd44f9780d85117fb14f9c4cd96ed
Anna Weisbrodt <aweisbro@gmail.com>
2016-07-20 18:18:26 -0600

    fav merchant for customer

--- spec/requests/api/v1/customers_BI_spec.rb
+++ spec/requests/api/v1/customers_BI_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 describe "Customers BI Endpoints" do
   it "returns favorite merchant for customer" do
-    customer = create(:customer)
+    merchant = create(:merchant, name: "Bob")
+    customer = create(:customer, :with_transactions, number_of: 5, merchant_id: merchant.id)
+
     get "/api/v1/customers/#{customer.id}/favorite_merchant"
 
     expect(response).to be_success
+  
   end
 end
